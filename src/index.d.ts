a7799b627d6384f3702c6a208ad4940a90ca4dd7
LLH <youyeke@163.com>
2018-12-03 08:51:02 +0800

    补充 index.d.ts

---
+++ src/index.d.ts
@@ -0,0 +1,11 @@
+import Flex from './components/Flex';
+import Grid from './components/Grid';
+import InlineScrollLayout from './components/InlineScrollLayout';
+import LRLayout from './components/LRLayout';
+
+export {
+  Flex,
+  Grid,
+  InlineScrollLayout,
+  LRLayout,
+};
